0d3314c182e5f24193cbd0a1198d63110217fb2b
Klyde-Sangalang <kvbsangalang.student@ua.edu.ph>
2025-08-31 23:40:04 +0800

    update

--- frontend/src/pages/LoginSignUp.jsx
+++ frontend/src/pages/LoginSignUp.jsx
@@ -16,6 +16,21 @@ export const LoginSignUp = () => {
 
   const login = async () => {
     console.log("login", formData);
+    let responseData;
+    await fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }).then((response)=> response.json()).then((data)=>responseData = data);
+
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.error);
+    }
   }
   const signup = async () => {
     console.log("signup", formData);
